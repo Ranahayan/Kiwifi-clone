b5d3e59f60ed2b223a4abdfa8a4c5cee1976ea39
Ranahayan <2019cs237@student.uet.edu.pk>
2023-03-12 10:25:34 +0500

    Only validation for password remained incomplete

--- kiwifi-clone/src/ReuseableForm/Input.jsx
+++ kiwifi-clone/src/ReuseableForm/Input.jsx
@@ -1,12 +1,25 @@
 import { rest } from "lodash";
 import React from "react";
+import "../styles/style.css";
 
 const InputFeild = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group mb-3">
       <label htmlFor={name}>{label}</label>
-      <input {...rest} id={name} name={name} className="form-control" />
-      {error && <div className="alert alert-danger mt-1">{error}</div>}
+      <input
+        {...rest}
+        id={name}
+        name={name}
+        autoComplete="new-password"
+        className={
+          error ? "form-control regisetr-inputBorderForError" : "form-control"
+        }
+      />
+      {error && (
+        <div className="text-danger mt-1">
+          <p className="regisetr-errorMessage">{error}</p>
+        </div>
+      )}
     </div>
   );
 };
