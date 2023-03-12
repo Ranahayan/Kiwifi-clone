f39593f8362a9c9596624baf1668a1818b2f8877
Ranahayan <2019cs237@student.uet.edu.pk>
2023-03-12 14:15:32 +0500

    validations on login has added

--- kiwifi-clone/src/Pages/Login.js
+++ kiwifi-clone/src/Pages/Login.js
@@ -23,17 +23,13 @@ export default function Login() {
         "string.empty": "This feild is mendatory",
         "string.pattern.base": "Invalid email formate",
         "string.email": "Invalid email format",
+        "any.required": "This feild is mendatory",
       })
       .label("Email"),
-    password: Joi.string()
-      .min(8)
-      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).*$/)
-      .required()
-      .messages({
-        "string.pattern.base":
-          "Password must contain at least one uppercase letter, one lowercase letter,\n one digit, one special character, and be at least 8 characters long",
-        "string.empty": "This feild is mendatory",
-      }),
+    password: Joi.string().required().messages({
+      "string.empty": "This feild is mendatory",
+      "any.required": "This feild is mendatory",
+    }),
   };
   const schema = Joi.object(validations);
 
@@ -74,19 +70,7 @@ export default function Login() {
           >
             <a style={{ fontSize: "0.875rem" }}>Forgot Password?</a>
           </div>
-          <div class="form-check terms">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="termsCheckbox"
-            />
-            <label class="form-check-label" for="termsCheckbox">
-              I have read and accept kiwify's <a>terms</a> of use ,{" "}
-              <a>software license terms</a> ,<a>content policy</a>.
-            </label>
-            <div className="checkboxError"></div>
-          </div>
+
           {renderButton("To enter")}
         </form>
       </div>
