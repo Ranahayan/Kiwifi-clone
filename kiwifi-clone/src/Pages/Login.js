c930527ab26aa9c63be2d35303eef2f1c57f0e73
ehtashamhaider07 <ehaider822@gmail.com>
2023-03-12 10:24:58 +0500

    Upper part of forms is ready

--- kiwifi-clone/src/Pages/Login.js
+++ kiwifi-clone/src/Pages/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
 import '../styles/loginPage.css';
+import logoImg from '../assets/kiwify-logo.png';
 
 
 
 export default function Login() {
   return <div className="loginPage">
-      <img src="../assets/kiwify-logo.png" alt="" />
-    </div>;
+    <div className="contentArea d-flex flex-column align-items-center">
+      <img src={logoImg} alt="" />
+      <span className="heading-1">Create new account</span>
+      <p>Or <a>log in to your existing account</a></p>
+    </div>
+  </div>;
 }
